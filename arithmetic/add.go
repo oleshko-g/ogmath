982fb46b4698b1c7462d14cc817e8ad09ca65f0e
Gennady Oleshko <gennady@oleshko-g.ru>
2025-11-05 09:59:48 +0300

    make Add generic

--- arithmetic/add.go
+++ arithmetic/add.go
@@ -1,7 +1,14 @@
 // Package arithmetic provides functions to make do arithmetic
 package arithmetic
 
+import "golang.org/x/exp/constraints"
+
+// Number is a constraint for math operations
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add return the sum of a and b
-func Add(a, b int) int {
+func Add[N Number](a, b N) N {
 	return a + b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/oleshko-g/ogmath
+module github.com/oleshko-g/ogmath/v2
 
 go 1.25.3
+
+require golang.org/x/exp v0.0.0-20251023183803-a4bb9ffd2546
